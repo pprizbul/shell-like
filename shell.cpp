6588be231abb826ab9f90643f3d8d456b38363fe
Degrad21 <xprizbul@stuba.sk>
2026-07-22 22:10:43 +0200

    feat(shell.cpp): rewrote as class

--- shell.cpp
+++ shell.cpp
@@ -1,10 +1,61 @@
 #include <array>
 #include <iostream>
+#include <memory>
 #include <asio.hpp>
 
-void handle_read(const std::error_code& e, 
-    size_t bytes_transfered, std::string *recieved_message);
-void handle_write(asio::ip::tcp::socket *socket);
+class client : public std::enable_shared_from_this<client>{
+public:
+    typedef std::shared_ptr<client> client_ptr;
+
+    static client_ptr create(asio::io_context& io_context, 
+        asio::ip::tcp::resolver::results_type& endpoints){
+            return client_ptr(new client(io_context, endpoints));
+        }
+
+    void start(){
+        do_connect();
+    }
+private:
+    client(asio::io_context& io_context, 
+        asio::ip::tcp::resolver::results_type& endpoints)
+    :  io_context_(io_context),
+        endpoints_(endpoints),
+        socket_(io_context)
+        {
+        }
+
+    void do_connect(){
+        asio::connect(socket_, endpoints_);
+        do_write();
+    }
+
+    void do_write(){
+        auto self(shared_from_this());
+        std::string imessage{"test_message"};
+        std::cout << "Writing message " << imessage << std::endl;
+       
+        socket_.async_write_some(asio::buffer(imessage),
+        [this, self](const asio::error_code, std::size_t){
+            std::cout << "Message sent" << std::endl;
+            do_read();
+        });
+    }
+
+    void do_read(){
+        auto self(shared_from_this());
+        socket_.async_read_some(asio::buffer(buffer_),
+        [this, self](asio::error_code, std::size_t bytes_transfered){
+            message_.assign(buffer_.data(), bytes_transfered);
+            std::cout << "Recieved message: " << message_ << std::endl;
+        });
+    }
+
+    asio::io_context& io_context_;
+    asio::ip::tcp::resolver::results_type& endpoints_;
+    asio::ip::tcp::socket socket_;
+    std::array<char, 128> buffer_;
+    std::string message_;
+};
 
 int main(int argc, char *argv[]) {
     try{
@@ -17,34 +68,13 @@ int main(int argc, char *argv[]) {
         asio::ip::tcp::resolver resolver(io_context);
         asio::ip::tcp::resolver::results_type endpoints =
             resolver.resolve(argv[1], "1301");
-
-        asio::ip::tcp::socket socket(io_context);
-        asio::connect(socket, endpoints);
-
-        std::array<char, 128> buf;
-        std::error_code error;
-
-        socket.async_write_some(asio::buffer("test"), 
-            std::bind(&handle_write, &socket));
         
+        client::client_ptr new_client = client::create(io_context, endpoints);
+        new_client->start();
         io_context.run();
     }
     catch(std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
     return 0;
-}
-
-void handle_read(const std::error_code& e, 
-    size_t bytes_transfered, std::string *recieved_message){
-    std::cout << "Recieved message" << std::endl;
-}
-
-void handle_write(asio::ip::tcp::socket *socket){
-    std::cout << "Writing message " << "test" << std::endl;
-
-    std::string recieved_message;
-    socket->async_read_some(asio::buffer(recieved_message),
-        std::bind(&handle_read, asio::placeholders::error, 
-            asio::placeholders::bytes_transferred, &recieved_message));        
 }
